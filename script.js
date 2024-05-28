807daf36024f8a60f7918d55262aac75f43cf39d
Artemisa Ruci <artemisa.ruci@gmail.com>
2024-05-28 20:52:45 +0200

    Change Canvas Resolution on User Input

--- script.js
+++ script.js
@@ -1,22 +1,55 @@
 // General Variables
 const canvas = document.querySelector(".canvas");
-const cellPerSide = 16;
-const cellNumber = Math.pow(cellPerSide, 2);
 
 const modal = document.querySelector(".modal");
 const resizeBtn = document.querySelector("#resize");
-const closeModalBtn =  document.querySelector("#closeModal");
+const closeModalBtn = document.querySelector("#closeModal");
+const cancelBtn = document.querySelector("#cancelBtn");
+const saveBtn = document.querySelector("#saveBtn");
 
-// Canvas Grid
-for (let i = 0; i < cellNumber; i++) {
-    const canvasCell = document.createElement("div");
-    canvasCell.setAttribute("class", "canvas-cell");
+// Set Canvas Grid Size
+function createGrid() {
+    let inputValue = document.querySelector("#squaresPerSide").value;
+    canvasGrid(inputValue);
+}
 
-    canvas.appendChild(canvasCell);
+// Remove Canvas Grid
+function removeGrid() {
+    let leftOvers = canvas.firstChild;
+    while (leftOvers) {
+        canvas.removeChild(leftOvers);
+    }
+}
 
-    canvasCell.addEventListener("mouseover", () => {
-        canvasCell.style.background = "#141414";
-    });
+// Create Canvas Grid
+function canvasGrid(squaresPerSide) {
+    const width = `calc(var(--canvasWidth) / ${squaresPerSide})`;
+    const height = width;
+    const squares = Math.pow(squaresPerSide, 2);
+    for (let n = 0; n < squares; n++) {
+        canvasCell(width, height);
+    }
+}
+
+// Remove Canvas Grid
+function removeGrid() {
+    while (canvas.firstChild) {
+        canvas.removeChild(canvas.lastChild);
+    }
+}
+
+// Create Grid Square
+function canvasCell(squareWidth, squareHeight) {
+    const square = document.createElement("div");
+    square.style.width = squareWidth;
+    square.style.height = squareHeight;
+
+    canvas.appendChild(square);
+
+    // Add different styles
+    square.addEventListener("mouseover", () => {
+        square.style.background = "#1a1a1a";
+    })
 }
 
 // Show Modal
@@ -30,10 +63,21 @@ function hideModal() {
 }
 
 // Event Listeners
+window.addEventListener("load", () => {createGrid()});
 resizeBtn.addEventListener("click", () => {showModal()});
 closeModalBtn.addEventListener("click", () => {hideModal()});
 window.addEventListener("click", (event) => {
     if (event.target === modal) {
         hideModal();
     }
+});
+
+saveBtn.addEventListener("click", () => {
+    removeGrid();
+    hideModal();
+    createGrid();
+});
+
+cancelBtn.addEventListener("click", () => {
+    hideModal();
 });
