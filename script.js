708305aa46b07b878b2f7b075372115c06ea2cfc
Artemisa Ruci <artemisa.ruci@gmail.com>
2024-05-31 01:33:11 +0200

    Implement Drawing Styles

--- script.js
+++ script.js
@@ -1,18 +1,38 @@
 // General Variables
 const canvas = document.querySelector(".canvas");
 
+// Resolution Modal
 const modal = document.querySelector(".modal");
-const resizeBtn = document.querySelector("#resize");
+
 const closeModalBtn = document.querySelector("#closeModal");
 const cancelBtn = document.querySelector("#cancelBtn");
 const saveBtn = document.querySelector("#saveBtn");
 
+// Confirm Clear Modal - To be implemented
+
+// Buttons => Implement Toggle Grid View
+const pencil = document.querySelector("#pencil");
+const eraser = document.querySelector("#eraser");
+const resize = document.querySelector("#resize");
 const colorPicker = document.querySelector("#colorPicker");
+const clear = document.querySelector("#clear");
+
+// Drawing Styles Buttons (Filters)
+const monoColor = document.querySelector("#monoColor");
+const randomColors = document.querySelector("#random");
+const addOpacity = document.querySelector("#addOpacity");
+
+let sketching = false;
+let erasing = false;
+let randomizing = false;
+let darkening = false;
+
+let pencilColor = "#1a1a1a";
 
 // Create Canvas Grid
 function createGrid() {
     let inputValue = document.querySelector("#squaresPerSide").value;
-    canvasGrid(inputValue);
+    gridSquares(inputValue);
 }
 
 // Remove Canvas Grid
@@ -23,17 +43,17 @@ function removeGrid() {
 }
 
 // Generate Grid Squares
-function canvasGrid(squaresPerSide) {
+function gridSquares(squaresPerSide) {
     const width = `calc(var(--canvasWidth) / ${squaresPerSide})`;
     const height = width;
     const squares = Math.pow(squaresPerSide, 2);
     for (let n = 0; n < squares; n++) {
-        canvasCell(width, height);
+        createSquare(width, height);
     }
 }
 
 // Create Grid Square
-function canvasCell(squareWidth, squareHeight) {
+function createSquare(squareWidth, squareHeight) {
     const square = document.createElement("div");
     square.style.width = squareWidth;
     square.style.height = squareHeight;
@@ -42,45 +62,35 @@ function canvasCell(squareWidth, squareHeight) {
 
     // Add different styles
     square.addEventListener("mouseover", () => {
-        for (let a = 10; a <= 100; a++) {
-            let colour = `rgba(0, 0, 0, ${a/100})`;
-            square.style.background = colour;    
-        } 
+        addShade(square);
     })
 }
 
 // Drawing Styles
 
 // Mono Color
-function monoColor() {
-    colorPicker.addEventListener("input", (e) => {
-        let color = e.target.value;
-        return color;
-    })
+function singleColor(monoBg) {
+    monoBg.style.background = colorPicker.value;
 }
 
 // Random Color
-function randomColor() {
+function randomColor(randomBg) {
     const red = Math.floor(Math.random() * 256);
     const green = Math.floor(Math.random() * 256);
     const blue = Math.floor(Math.random() * 256);
     const random = `rgb(${red}, ${green}, ${blue})`;
-    return random;
+    randomBg.style.background = random;
 }
 
 // Add Shade
-function addShade() {
-    let shadePercentage = 0;
-    let intValue = 0;
-    let shadeHex = 0;
-    while (shadePercentage < 100) {
-        shadePercentage += 10;
-        intValue = Math.round(shadePercentage / 100 * 255);
-        shadeHex = intValue.toString(16);
-        console.log(shadeHex)
+function addShade(transparentBg) {
+    transparentBg.style.background = colorPicker.value;
+    let opacity = Number(transparentBg.style.opacity);
+    if (opacity < 1) {
+        opacity += 0.1;
+        transparentBg.style.opacity = opacity;
     }
 }
-addShade();
 
 // Show Modal
 function showModal() {
@@ -94,7 +104,7 @@ function hideModal() {
 
 // Event Listeners
 window.addEventListener("load", () => {createGrid()});
-resizeBtn.addEventListener("click", () => {showModal()});
+resize.addEventListener("click", () => {showModal()});
 closeModalBtn.addEventListener("click", () => {hideModal()});
 window.addEventListener("click", (event) => {
     if (event.target === modal) {
